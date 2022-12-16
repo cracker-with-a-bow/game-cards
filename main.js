3505f4a8de650e9111335b0303a7a5742062dabc
cracker-with-a-bow <cracker.with.a.bow@gmail.com>
2022-12-16 03:25:15 +0300

    add li in container

--- main.js
+++ main.js
@@ -1,33 +1,36 @@
 (function () {
-
-  function createCountCards () {
+  let selectValue = 4;
+  function createCountCards() {
     let select = document.querySelector('.modal-window__select');
-
-select.addEventListener('change', function e(){
-  let selectValue = select.value;
-  console.log(selectValue);
-})
-console.log(select);
+let modalWindow = document.querySelector('.modal-window');
+    select.addEventListener('change', function e() {
+      selectValue = parseInt(select.value);
+      modalWindow.classList.add('display-none');
+      createGameField(selectValue);
+    })
+    return selectValue;
 
   }
 
   createCountCards();
 
   function createGameField(countCards) {
-    parseInt(countCards);
-    console.log(countCards);
+    countCards = selectValue;
     let list = document.createElement('ul');
     list.classList.add('list-group');
+    list.classList.add('cards');
     let item;
-    for (let i = 0; i <= countCards; ++i) {
+    for (let i = 0; i < countCards; ++i) {
       item = document.createElement('li');
-      console.log(li);
+      item.classList.add('card');
       list.append(item);
     };
     console.log(list);
-    let container = document.getElementById('game');
-    container.append(list);
+    let cardsContainer = document.getElementById('cards-container');
+    cardsContainer.append(list);
   }
+
+
 
   /*function createTodoApp(container, title, keyName) {
 
