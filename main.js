e6419a6cb4be832c7b2b88daa04e6cb3c538dc49
cracker-with-a-bow <cracker.with.a.bow@gmail.com>
2023-04-25 01:03:41 +0300

    shuffleArray, click on cards, textContent in cards

--- main.js
+++ main.js
@@ -1,39 +1,66 @@
 (function () {
+
   let selectValue = 4;
   function createCountCards() {
     let select = document.querySelector('.modal-window__select');
-let modalWindow = document.querySelector('.modal-window');
+    let modalWindow = document.querySelector('.modal-window');
     select.addEventListener('change', function e() {
       selectValue = parseInt(select.value);
       modalWindow.classList.add('display-none');
       createGameField(selectValue);
     })
     return selectValue;
-
   }
 
   createCountCards();
 
   function createGameField(countCards) {
-    countCards = selectValue;
+    createArrayNumbersCards(selectValue);
     let list = document.createElement('ul');
     list.classList.add('list-group');
     list.classList.add('cards');
-    let item;
+    let card;
     for (let i = 0; i < countCards; ++i) {
-      item = document.createElement('li');
-      item.classList.add('card');
-      item.classList.add('col-2');
-      list.append(item);
+      card = document.createElement('li');
+      card.classList.add('card');
+      card.classList.add('col-2');
+      cardText = document.createElement('p');
+      cardText.classList.add('card-text');
+      cardText.textContent = arrayNumbersCards[i];
+      card.append(cardText);
+      list.append(card);
+      card.addEventListener('click', clickCard)
     };
-    console.log(list);
+
     let cardsContainer = document.getElementById('cards-container');
     cardsContainer.append(list);
+    return cardText;
   }
 
-  function shuffleArray(countItems) {
-    countItems = selectValue;
-    arrayOrigin = [];
+
+  function createArrayNumbersCards(numbersCard) {
+    arrayNumbersCards = [];
+    for (let i = 0; i < (numbersCard / 2); ++i) {
+      arrayNumbersCards.push(i);
+    };
+    arrayNumbersCards = arrayNumbersCards.concat(arrayNumbersCards);
+    shuffleArray(arrayNumbersCards);
+    return arrayNumbersCards;
+  }
+
+
+
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    };
+    return array;
+  }
+
+  function clickCard(e) {
+     let activeCard = e.target;
+     console.log(activeCard.textContent);
   }
 
 
